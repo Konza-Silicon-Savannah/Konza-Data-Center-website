aae98edd874c4678c4d56eb35d7b2a29ae5c0523
google-boy <otienoh13@gmail.com>
2023-03-18 18:38:00 +0000

    Typo correction

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         <div class="container header-text">
             <h1>One Stop Shop for all your cloud needs.</h1>
             <p>
-                Are you in need of web hosting using the conventional cpanel or are you looking for fast, optimized and processor accelearted compute option? Look no further. Konza cloud has all these offerings provisioned for you as easy as pie
+                Are you in need of web hosting using the conventional cpanel or are you looking for fast, optimized and processor accelerated compute option? Look no further. Konza cloud has all these offerings provisioned for you as easy as pie
             </p>
             <button type="button" class="btn btn-outline-success mt-3" data-bs-toggle="modal" data-bs-target="#callbackmodal">
                 Request a Call Back
